47bf2fb53a7a0859ce8b8109c0b608fa22dbe0a1
roberto <roberto@interdata.com.ar>
2015-11-14 08:54:43 -0300

    x

---
+++ robertoGit/forms/formInicio.js
@@ -0,0 +1,11 @@
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"9174D09A-2986-46B2-86F6-67D7EDD14146"}
+ */
+function onActionSalir(event) {
+	application.output('Saliendo...')
+}
